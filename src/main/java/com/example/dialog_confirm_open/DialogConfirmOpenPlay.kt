1efa020c1523a4e75ae57f26796db48b525c379e
Nguyen Van Doan OS <vandoannguyenhaui@gmail.com>
2022-04-07 07:48:35 +0700

    converting

--- src/main/java/com/example/dialog_confirm_open/DialogConfirmOpenPlay.kt
+++ src/main/java/com/example/dialog_confirm_open/DialogConfirmOpenPlay.kt
@@ -3,18 +3,20 @@ package com.example.dialog_confirm_open
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.example.ratedialog.R
-import kotlinx.android.synthetic.main.dialog_confirm_chplay.*
+import com.example.ratedialog.databinding.DialogConfirmChplayBinding
 
 class DialogConfirmOpenPlay(context: Context, private val clickYes: ClickYes) : Dialog(context) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.dialog_confirm_chplay)
-        linearYesChPlay.setOnClickListener {
+        val binding = DialogConfirmChplayBinding.inflate(LayoutInflater.from(context))
+        setContentView(binding.root)
+        binding.linearYesChPlay.setOnClickListener {
             clickYes.onClick()
             dismiss()
         }
-        linearNoChPlay.setOnClickListener {
+        binding.linearNoChPlay.setOnClickListener {
             dismiss()
         }
     }
